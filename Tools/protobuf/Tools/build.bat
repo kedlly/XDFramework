57caca4782feaaeaf43897ad5e8db33329f54ef6
kedlly <zhaoxh6525@163.com>
2018-08-15 19:47:55 +0800

    update

--- Tools/protobuf/Tools/build.bat
+++ Tools/protobuf/Tools/build.bat
@@ -1,10 +1,10 @@
 
-
-set DIR=%cd%\..\Protocals
-set CS_OUT=..\..\..\Assets\Scripts\Protocals\
-set PY_OUT=..\..\..\GameServer\Messages\
+set BAT_DIR=%~dp0
+set DIR=%BAT_DIR%\..\Protocals
+set CS_OUT=%BAT_DIR%\..\..\..\Assets\Scripts\Protocals\
+set PY_OUT=%BAT_DIR%\..\..\..\GameServer\Messages\
 set Files=Messages.proto RequestMessages.proto RespondMessages.proto
 
 rem .\Windows\protogen -I..\Protocals\ +langver=4.0 +names=original --csharp_out=..\..\..\Assets\Scripts\Protocals\ %Files%
-.\Windows\protogen -I%DIR% +langver=4.0 +names=original --csharp_out=%CS_OUT% %Files%
+%BAT_DIR%\Windows\protogen -I%DIR% +langver=4.0 +names=original --csharp_out=%CS_OUT% %Files%
 protoc.exe -I%DIR% --python_out=%PY_OUT% %Files%
