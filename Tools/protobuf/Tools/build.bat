07846e1d83613445ab994392de3a77c6cf6c66b9
kedlly <zhaoxh6525@163.com>
2018-08-15 19:40:09 +0800

    update

--- Tools/protobuf/Tools/build.bat
+++ Tools/protobuf/Tools/build.bat
@@ -1,5 +1,10 @@
 
-.\Windows\protogen -I..\Protocals\ +langver=4.0 +names=original --csharp_out=..\..\..\Assets\Scripts\Protocals\ *.proto
 
-set DIR=D:\GitHub\XDFramework\Tools\protobuf\Protocals\
-protoc.exe -I%DIR% --python_out=..\..\..\GameServer\Messages\ Messages.proto
+set DIR=%cd%\..\Protocals
+set CS_OUT=..\..\..\Assets\Scripts\Protocals\
+set PY_OUT=..\..\..\GameServer\Messages\
+set Files=Messages.proto RequestMessages.proto RespondMessages.proto
+
+rem .\Windows\protogen -I..\Protocals\ +langver=4.0 +names=original --csharp_out=..\..\..\Assets\Scripts\Protocals\ %Files%
+.\Windows\protogen -I%DIR% +langver=4.0 +names=original --csharp_out=%CS_OUT% %Files%
+protoc.exe -I%DIR% --python_out=%PY_OUT% %Files%
