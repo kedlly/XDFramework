4115a56bb7e2eb680c1f876ecbb31d3c6357099a
kedlly <zhaoxh6525@163.com>
2018-08-30 09:51:52 +0800

    update

--- GameServer/Server.py
+++ GameServer/Server.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
 	config = Configure.getConfigure("config.ini")
 	ipAddress = config['server'][0]
 	port = int(config['server'][1])
+	print "listening: %s, port : %d" % (ipAddress, port)
 	LogicServer(port).start()
